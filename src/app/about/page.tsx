4086e1f172d54865a24adcaeae45abdfe49604cb
Mohammed Al-Hashimi <malhash2@uwo.ca>
2025-08-08 20:07:19 -0400

    created a couple pages and added some content to about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+export default function About() {
+	return (
+		<section className="py-20 px-6 max-w-3xl mx-auto" id="about">
+			<h2 className="text-3xl font-bold mb-4">About Me</h2>
+			<p className="text-gray-700 leading-relaxed">
+				I'm a 4th-year Software Engineering student with experience in building responsive web apps using React, Node.js, and more.
+				I enjoy creating clean UIs, solving logical problems, and learning new technologies.
+			</p>
+		</section>
+	)
+}
